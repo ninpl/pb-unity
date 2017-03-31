0e73d9b912ee59da646b8b1a289206fe9061558d
Moon Antonio <pinchodev91@gmail.com>
2017-03-31 04:29:28 +0200

    PB Radial iniciado

---
+++ PB Project/Assets/Proyecto PB/Scripts/PBManager.cs
@@ -0,0 +1,20 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// PBManager.cs (00/00/0000)													\\
+// Autor: Antonio Mateo (Moon Antonio) 									        \\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio
+{
+	public class PBManager : MonoBehaviour 
+	{
+
+	}
+}
